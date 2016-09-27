186af2f750f68189d5f4e0409340c2eb9a25916c
kezarberger <kezarberger@hanovernorwichschools.org>
2016-09-27 14:00:01 -0400

    Updated from Brython Server: 9/27/2016 2:00:00 PM

--- birthday.py
+++ birthday.py
@@ -35,7 +35,20 @@ name = input("Hello, what is your name? ")
 month = input("Hi " + name + ", What was the name of the month you were born in? ")
 year = input("And what year were you born in, " + name + "? ")
 day = input("And the day? ")
-if month in ["December, January, Febuary"]: 
-    season = ["Winter"]
-if month in ["March, April, May,"]:
-    season = ["Spring"]
+if month in ["December", "January", "Febuary"]: 
+    season = "winter"
+if month in ["March", "April", "May"]:
+    season = "spring"
+if month in ["June", "July", "August"]:
+    season = "summer"
+if month in ["September", "October", "November"]:
+    season = "fall"
+if year in ["1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979"]:
+    decade = "stone age"
+if year in ["1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989"]:
+    decade = "eighties"
+if year in ["1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999"]:
+    decade = "nineties"
+if year in ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009"]:
+    decade = "21st century"
+print (name + ", you are a " + str(season) + " baby of the " + str(decade) + ".")
