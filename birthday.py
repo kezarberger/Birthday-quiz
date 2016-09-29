ea4177308c582e2246fcf3c0c84ca0d56ab4340a
kezarberger <kezarberger@hanovernorwichschools.org>
2016-09-29 13:37:48 -0400

    Updated from Brython Server: 9/29/2016 1:37:47 PM

--- birthday.py
+++ birthday.py
@@ -43,14 +43,12 @@ if mymonth in ["June", "July", "August"]:
     season = "summer"
 if mymonth in ["September", "October", "November"]:
     season = "fall"
-if year in ["1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979"]:
-    decade = "stone age"
 if year in ["1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989"]:
     decade = "eighties"
 if year in ["1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999"]:
     decade = "nineties"
-if year in ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009"]:
-    decade = "21st century"
+if year in ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015"]:
+    decade = "stone age"
 from datetime import datetime
 from calendar import month_name
 todaymonth = datetime.today().month
